ec399ceb4e19cc96b6ae08e72c8b7f28aec1959b
霍振鹏 <huozhenpengdyx@gmail.com>
2020-06-26 10:22:45 +0800

    softsoft

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -76,5 +76,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("111111");
         System.out.println("222222");
         System.out.println("333333");
+        System.out.println("soft soft");
     }
 }
